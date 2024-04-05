ef34812a1d100ffc6b8401701430fe448e3ec8a0
XComatoseX <denismanko2006@gmail.com>
2024-04-06 00:17:34 +0300

    22

--- js/checkUser.js
+++ js/checkUser.js
@@ -1,19 +1,6 @@
-function isMobileDevice() {
-  return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
-}
 
-// Проверяем, является ли устройство Samsung A52
-function isSamsungA52() {
-  // Можно попробовать определить устройство по строке user-agent
-  return navigator.userAgent.includes("Samsung A52");
-}
+  modelInfo.textContent = window.navigator.userAgent;
 
-if (isMobileDevice() && isSamsungA52()) {
-  modelInfo.textContent = "ДААА";
-} else {
-  modelInfo.textContent = "Unknown";
-  console.log("Пользователь не зашел с телефона Samsung A52 или не с мобильного устройства");
-}
 
 
 
